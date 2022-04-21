2aca7b641b41a1555230232d164971e22a5e978a
Tenzin Choezom <khortsa2021@gmail.com>
2022-04-21 16:42:20 +0200

    first Mongoose

---
+++ index.js
@@ -0,0 +1,46 @@
+const mongoose= require("mongoose");
+mongoose.connect("mongodb://localhost:27017/populate");
+const Schema = mongoose.Schema;
+
+
+const studentSchema = Schema({
+    firstName: String,
+    lastName: String,
+    address: {
+        type: mongoose.Types.ObjectId,
+        ref: "Address"
+    }
+});
+const Student = mongoose.model('Student', studentSchema);
+const addressSchema = Schema({
+    streetName: String,
+    streetNumber: String,
+    postCode: String,
+    city: String
+});
+const Address = mongoose.model('Address', addressSchema);
+
+const studentAddress = new Address({
+    streetName: "rue de la reunion",
+    streetNumber: "15",
+    postCode: "75020",
+    city: "Paris" 
+});
+// studentAddress.save().then(function (result){
+//     const personA= new Student({
+//         firstName: "Jean",
+//         lastName: "Guo",
+//         address: result._id
+//     });
+//     personA.save((err) =>{
+
+//     });
+// });
+
+Student
+.findOne({_id: "6261600bf4e9b086ec3e7dfb"})
+.populate('address')
+.exec((err, student) =>{
+    console.log("This is", student);
+});
+
